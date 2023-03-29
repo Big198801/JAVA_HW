0b518efaf40baa4bb46cee67e92e54e1583d6f77
NikolayAfanasev <bigniko@mail.ru>
2023-03-29 22:15:17 +0300

    task 3 done

---
+++ hw_lesson_1/task_3.java
@@ -0,0 +1,46 @@
+/*
+ * Task_3. 
+Реализовать простой калькулятор (+,-,=,*), только с целыми числами.
+ */
+package JAVA.HW_JAVA.hw_lesson_1;
+import java.util.Scanner;
+public class task_3 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        while (true) {
+        System.out.println("+ для сложения\n- для вычитания\n* для умножения\n/ для деления\n= для получения результата");
+        System.out.println("введите первое число:");
+        int x = scn.nextInt(); 
+        System.out.println("операция ....");
+        char oper = scn.next().charAt(0);       
+        System.out.println("введите второе число: ");
+        int y = scn.nextInt();
+        int result = 0;
+        switch (oper) {
+            case ('-'):
+                result = x - y;
+                break;
+            case('+'):
+                result = x + y;
+                break;
+            case('*'):
+                result = x * y;
+                break;
+            case('/'):
+                result = x / y;
+                break;
+        
+            default:
+                break;
+        }
+        
+        char res = scn.next().charAt(0);
+        if (res == '=') {
+            System.out.println(result);
+        }
+
+        }  
+        
+    }
+    
+}
