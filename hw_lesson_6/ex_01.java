2a231f088be37e7da6e1f3d098e1d6ccc7d2de1d
NikolayAfanasev <bigniko@mail.ru>
2023-04-17 22:52:04 +0300

    added мар  для сохранения критериев поиска

--- hw_lesson_6/ex_01.java
+++ hw_lesson_6/ex_01.java
@@ -1,7 +1,11 @@
 package HW_JAVA.hw_lesson_6;
 import HW_JAVA.hw_lesson_6.Notebook;
+
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.Scanner;
 
 public class ex_01 {
     public static void main(String[] args) {
@@ -13,7 +17,51 @@ public class ex_01 {
         notebooks.add(new Notebook("Asus", "Rog13", 16, 512, "noOS", "black", 999));
         notebooks.add(new Notebook("HP", "Envy14", 16, 256, "Windows 11", "Blue", 799));
         viewAll(notebooks);
+                
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n\tкритерии выбора по: ");
+        System.out.println("1 - ОЗУ");
+        System.out.println("2 - HDD/SSD");
+        System.out.println("3 - Операционная система");
+        System.out.println("4 - Марка");
+        System.out.println("\tили 0 для завершения ввода:  ");
         
+        Map<String,Object> critery = new HashMap<>();
+        int choice = sc.nextInt();
+        while (choice > 0 && choice < 5) {
+            switch (choice) {
+                case (1):
+                    System.out.println("введите минимальный объем ОЗУ:");
+                    int ram = sc.nextInt();
+                    critery.put("ram", ram);    
+                    break;
+                case (2):
+                    System.out.println("введите минимальный объем HDD/SSD:");
+                    int pzu = sc.nextInt();
+                    critery.put("storage", pzu);
+                    break;  
+                case (3):
+                    System.out.println("необходимая ОС: ");
+                    String os = sc.next();
+                    critery.put("storage", os);
+                    break;
+                case (4):
+                    System.out.println("введите производителя: ");
+                    String manufact = sc.next();
+                    critery.put("brand", manufact);
+                    break;
+                default:
+                    System.out.println("введен неверный номер критерия, повторите ввод");
+                    break;
+            }
+        System.out.println("Введите номер критерия (или 0 для завершения ввода):");
+        choice = sc.nextInt();
+        }
+        System.out.println("\nИспользуются следующие критерии для поиска:");
+        for (Map.Entry<String,Object> item : critery.entrySet()){
+            System.out.printf("%s: %s\n", item.getKey(), item.getValue() );
+                   
+        }
     }
     
     public static void viewAll(Set<Notebook>notebooks) {
@@ -24,5 +72,9 @@ public class ex_01 {
             }
         
     }
+    
+    
+
+
 }
 
